c138929fcfb5dcb05bfb3ed91991142370dc2e72
LovelyNMTech <130054738+LovelyNMTech@users.noreply.github.com>
2024-02-16 19:20:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ class Paper {
   currentPaperY = 0;
   rotating = false;
   init(paper) {
-    document.addEventListener('mousemove', (e) => {
+    const handleMove = (e) => {
       if(!this.rotating) {
-        this.mouseX = e.clientX;
-        this.mouseY = e.clientY;
+        this.mouseX = e.clientX || e.touches[0].clientX;
+        this.mouseY = e.clientY || e.touches[0].clientY;
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
       }
-      const dirX = e.clientX - this.mouseTouchX;
-      const dirY = e.clientY - this.mouseTouchY;
+      const dirX = (e.clientX || e.touches[0].clientX) - this.mouseTouchX;
+      const dirY = (e.clientY || e.touches[0].clientY) - this.mouseTouchY;
       const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
       const dirNormalizedX = dirX / dirLength;
       const dirNormalizedY = dirY / dirLength;
@@ -41,13 +41,13 @@ class Paper {
         this.prevMouseY = this.mouseY;
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
-    paper.addEventListener('mousedown', (e) => {
+    };
+    const handleDown = (e) => {
       if(this.holdingPaper) return;
       this.holdingPaper = true;
       paper.style.zIndex = highestZ;
       highestZ += 1;
-      if(e.button === 0) {
+      if(e.button === 0 || e.type === 'touchstart') {
         this.mouseTouchX = this.mouseX;
         this.mouseTouchY = this.mouseY;
         this.prevMouseX = this.mouseX;
@@ -56,11 +56,17 @@ class Paper {
       if(e.button === 2) {
         this.rotating = true;
       }
-    });
-    window.addEventListener('mouseup', () => {
+    };
+    const handleUp = () => {
       this.holdingPaper = false;
       this.rotating = false;
-    });
+    };
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('touchmove', handleMove);
+    paper.addEventListener('mousedown', handleDown);
+    paper.addEventListener('touchstart', handleDown);
+    window.addEventListener('mouseup', handleUp);
+    window.addEventListener('touchend', handleUp);
   }
 }
 const papers = Array.from(document.querySelectorAll('.paper'));
